881b092d70559e75592132741cb356ec4847f6e8
Сергей Авдеев <sergejavdeev@MacBook-Air-Sergej.local>
2023-06-04 04:54:33 +0700

    first commit

---
+++ src/main/java/com/example/ValueScannerBot/ValueScannerBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.ValueScannerBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValueScannerBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValueScannerBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ValueScannerBot/ValueScannerBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ValueScannerBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValueScannerBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
